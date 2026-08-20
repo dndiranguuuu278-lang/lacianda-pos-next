ca7552c978c28679af71faeeab485a26f7b2ce99
dndiranguuuu278-lang <dndiranguuuu278@gmail.com>
2026-08-20 03:49:48 +0300

    Remove Navbar import from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
 
 export default function TillPage() {
   const [inventory, setInventory] = useState<any[]>([]);
